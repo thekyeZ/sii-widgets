b00f33b864126698fc5e18e04031a56319c9cf2e
Sebastian Nowak <snowak1@sii.pl>
2023-02-09 09:32:40 +0100

    first steps done

--- src/app/widgets/weather/weather.module.ts
+++ src/app/widgets/weather/weather.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeatherComponent } from './weather.component';
+import { HeaderComponent } from './header/header.component';
+import { ListingComponent } from './listing/listing.component';
 
 
 
 @NgModule({
   declarations: [
-    WeatherComponent
+    WeatherComponent,
+    HeaderComponent,
+    ListingComponent
   ],
   imports: [
     CommonModule
