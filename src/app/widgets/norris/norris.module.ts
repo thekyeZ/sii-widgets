fdd274defd4270284db47004b07f8daffe855076
Tomasz Knitter <tknitter@sii.pl>
2023-02-09 09:12:29 +0100

    template components

--- src/app/widgets/norris/norris.module.ts
+++ src/app/widgets/norris/norris.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NorrisComponent } from './norris.component';
+import { HeaderComponent } from './header/header.component';
+import { MemComponent } from './mem/mem.component';
+import { MemListComponent } from './mem/mem-list/mem-list.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 
 
 @NgModule({
   declarations: [
-    NorrisComponent
+    NorrisComponent,
+    HeaderComponent,
+    MemComponent,
+    MemListComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
