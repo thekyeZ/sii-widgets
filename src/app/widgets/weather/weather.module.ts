d5c7fac1cbfdde4fd0655dc1718bb93cd0688820
Sebastian Nowak <snowak1@sii.pl>
2023-02-10 20:00:31 +0100

    cleaned and ready to work

--- src/app/widgets/weather/weather.module.ts
+++ src/app/widgets/weather/weather.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeatherComponent } from './weather.component';
-import { HeaderComponent } from './header/header.component';
-import { UpcomingDaysComponent } from './upcoming-days/upcoming-days.component';
-import { UpcomingDayComponent } from './upcoming-days/upcoming-day/upcoming-day.component';
 
 
 
 @NgModule({
   declarations: [
-    WeatherComponent,
-    HeaderComponent,
-    UpcomingDaysComponent,
-    UpcomingDayComponent
+    WeatherComponent
   ],
   imports: [
     CommonModule
