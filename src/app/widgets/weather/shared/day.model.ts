f4599d74cfcbe4168736e8442ed54618ab853b45
Sebastian Nowak <snowak1@sii.pl>
2023-02-20 12:48:58 +0100

    interface works

--- src/app/widgets/weather/shared/day.model.ts
+++ src/app/widgets/weather/shared/day.model.ts
@@ -1,3 +1,22 @@
-export class Day {
-    constructor(name: string, date: number, temperature: number, units: string,) {}
+
+export interface WeatherForTheDay {
+    latitude: number,
+    longitude: number,
+    generationtime_ms: number,
+    utc_offset_seconds: number,
+    timezone: string,
+    timezone_abbreviation: string,
+    elevation: number,
+    daily_units: DailyUnits,
+    daily: Daily
+}
+
+export interface DailyUnits {
+    time: string,
+    temperature_2m_max: string
+}
+
+export interface Daily {
+    time: string[],
+    temperature_2m_max: number[]
 }
--- src/app/widgets/weather/weather.component.ts
+++ src/app/widgets/weather/weather.component.ts
@@ -12,6 +12,7 @@ export class WeatherComponent implements OnInit{
   upcomingWeather = ['sunny', 'cold'];
 
   posts: any[] = [];
+
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
     this.requestPosts();
@@ -19,8 +20,8 @@ export class WeatherComponent implements OnInit{
 
   requestPosts() {
     // this.http.get<any>('https://jsonplaceholder.typicode.com/posts').subscribe(results => this.posts = results);
-    this.http.get<any>('https://api.open-meteo.com/v1/forecast?latitude=50.06&longitude=19.94&daily=temperature_2m_max&current_weather=true&timezone=Europe%2FBerlin').subscribe(results => console.log(results));
-    this.http.get<any>('https://api.open-meteo.com/v1/forecast?latitude=50.06&longitude=19.94&daily=temperature_2m_max&current_weather=true&timezone=Europe%2FBerlin').subscribe(results => this.posts = results.daily.temperature_2m_max);
+    this.http.get<any>('https://api.open-meteo.com/v1/forecast?latitude=50.06&longitude=19.94&daily=temperature_2m_max&timezone=Europe%2FBerlin').subscribe(results => console.log(results));
+    this.http.get<any>('https://api.open-meteo.com/v1/forecast?latitude=50.06&longitude=19.94&daily=temperature_2m_max&timezone=Europe%2FBerlin').subscribe(results => this.posts = results.daily.temperature_2m_max);
   }
   
   readMore(details: string) {
