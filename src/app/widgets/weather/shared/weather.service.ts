ae7f362055466850f346fed7b585e76456ccef98
Sebastian Nowak <snowak1@sii.pl>
2023-03-01 09:42:30 +0100

    details for current weather developed

--- src/app/widgets/weather/shared/weather.service.ts
+++ src/app/widgets/weather/shared/weather.service.ts
@@ -10,6 +10,8 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getWeatherData() {
-    return this.http.get<WeatherData>('https://api.open-meteo.com/v1/forecast?latitude=50.06&longitude=19.94&daily=temperature_2m_max&timezone=Europe%2FBerlin');
+    // return this.http.get<WeatherData>('https://api.open-meteo.com/v1/forecast?latitude=50.06&longitude=19.94&daily=temperature_2m_max&timezone=Europe%2FBerlin');
+    return this.http.get<WeatherData>('https://api.open-meteo.com/v1/forecast?latitude=50.06&longitude=19.94&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&timezone=Europe%2FBerlin');
+
   }
 }
