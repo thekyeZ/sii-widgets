5ca351f8261dc7e02c2dcc18339fd4e5662e3ec0
Przemysław Ren <przemyslaw.ren@gmail.com>
2023-03-06 08:57:50 +0100

    clicking on settings working

--- src/app/widgets/buses/panel-buses/panel-buses.component.ts
+++ src/app/widgets/buses/panel-buses/panel-buses.component.ts
@@ -1,29 +1,28 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { busStopNames } from '../model-buses/busStop.model';
-import { BusStopsService } from '../services-buses/bus-stops.service';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { busStopNames } from "../model-buses/busStop.model";
+import { BusStopsService } from "../services-buses/bus-stops.service";
 
 @Component({
-  selector: 'app-panel-buses',
-  templateUrl: './panel-buses.component.html',
-  styleUrls: ['./panel-buses.component.scss']
+  selector: "app-panel-buses",
+  templateUrl: "./panel-buses.component.html",
+  styleUrls: ["./panel-buses.component.scss"],
 })
 export class PanelBusesComponent {
   busStops!: busStopNames;
-  
+
   constructor(
     private busStopsService: BusStopsService,
     private router: Router
   ) {}
-  
+
   ngOnInit(): void {
     this.busStopsService
       .getBusStops()
       .subscribe((busStops) => (this.busStops = busStops));
   }
   onOpenSettings() {
-    this.router.navigate(["buses-admin"]);
+    this.router.navigate(["admin/buses"]);
   }
-
-
 }
+
