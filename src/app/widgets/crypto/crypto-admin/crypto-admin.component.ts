17c0e34febade353de0157392acf11461f12c96c
KamilKazmierczak666 <kkazmierczak6@sii.pl>
2023-03-07 09:34:41 +0100

    Added an input field in the setting

--- src/app/widgets/crypto/crypto-admin/crypto-admin.component.ts
+++ src/app/widgets/crypto/crypto-admin/crypto-admin.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-crypto-admin',
-  templateUrl: './crypto-admin.component.html',
-  styleUrls: ['./crypto-admin.component.scss']
+  selector: "app-crypto-admin",
+  templateUrl: "./crypto-admin.component.html",
+  styleUrls: ["./crypto-admin.component.scss"],
 })
 export class CryptoAdminComponent {
-
+  onAccept(value: string) {
+    console.log(value);
+  }
 }
