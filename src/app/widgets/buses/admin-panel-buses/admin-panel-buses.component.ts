d6aad9f83fe851315dee5a2954bd9c2f93468170
Przemysław Ren <przemyslaw.ren@gmail.com>
2023-03-10 01:31:09 +0100

    choosing incoming buses working

--- src/app/widgets/buses/admin-panel-buses/admin-panel-buses.component.ts
+++ src/app/widgets/buses/admin-panel-buses/admin-panel-buses.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { busStopNames } from "../model-buses/busStop.model";
+import { FetchBusStopsService } from "../services-buses/fetch-bus-stops.service";
 import { SelectedBusStopService } from "../services-buses/selected-bus-stop.service";
 
 @Component({
@@ -9,13 +10,29 @@ import { SelectedBusStopService } from "../services-buses/selected-bus-stop.serv
   templateUrl: "./admin-panel-buses.component.html",
   styleUrls: ["./admin-panel-buses.component.scss"],
 })
-export class AdminPanelBusesComponent {
-  defaultBusStop!: string;
+export class AdminPanelBusesComponent implements OnInit {
+  busStops: busStopNames = [];
+  selectedBusStopId!: string;
 
   constructor(
     private router: Router,
+    private fetchBusStopsService: FetchBusStopsService,
     private selectedBusStopService: SelectedBusStopService
   ) {}
+
+  ngOnInit(): void {
+    this.fetchBusStopsService
+      .fetchBusStops()
+      .subscribe((busStops) => (this.busStops = Object.values(busStops)));
+    this.selectedBusStopService.selectedBusStop.subscribe(
+      (id) => (this.selectedBusStopId = id)
+    );
+  }
+
+  onSelectBusStop(event: any) {
+    const id = event.target.value;
+    this.selectedBusStopService.updateSelectedBusStop(id);
+  }
 
   onSubmit() {
     this.router.navigate([""]);
--- src/app/widgets/buses/services-buses/selected-bus-stop.service.ts
+++ src/app/widgets/buses/services-buses/selected-bus-stop.service.ts
@@ -6,8 +6,10 @@ import { BehaviorSubject } from "rxjs";
 })
 export class SelectedBusStopService {
   selectedBusStop = new BehaviorSubject<string>("");
+  selectedBusStopName = new BehaviorSubject<string>("Choose your bus stop");
 
   updateSelectedBusStop(id: string): void {
     this.selectedBusStop.next(id);
+    
   }
 }
