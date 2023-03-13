5efbbb0c5c1ff9ccd2b27b1fa29a7dc29247647d
Tomasz Knitter <tknitter@sii.pl>
2023-03-13 08:54:08 +0100

    error chandling selectedMemObservable.service

--- src/app/widgets/norris/services/mem-http.service.ts
+++ src/app/widgets/norris/services/mem-http.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Post } from "../mem/interface/post.model";
 import { HttpClient } from "@angular/common/http";
 import { selectedMemService } from "./selectedMemObservable.service";
+import { catchError, throwError } from "rxjs";
 
 @Injectable({
   providedIn: "root",
@@ -12,21 +13,31 @@ export class MemHttpService {
     private memService: selectedMemService
   ) {}
 
-  apiForSelectedCategory: string = "https://api.chucknorris.io/jokes/random?category=";
+  apiForSelectedCategory: string =
+    "https://api.chucknorris.io/jokes/random?category=";
   apiForRandomCategory: string = "https://api.chucknorris.io/jokes/random?";
 
   requestForApi() {
     // choose this api if user select random cathegory (selectValue = "random")
-          if (this.memService.selectedCategory.value == "") {
-              return this.http.get<Post>(`${this.apiForRandomCategory}`);
-
-
-    } else {
-      
-  // choose this api if user select other cathegory (selectValue = "selectCathegory")
-           return this.http.get<Post>(
-              `${this.apiForSelectedCategory}${this.memService.selectedCategory.value}`
+    if (this.memService.selectedCategory.value == "") {
+      return this.http.get<Post>(`${this.apiForRandomCategory}`).pipe(
+        catchError((error: Response) => {
+          throw new Error(`Błąd sieci: ${error.statusText} (${error.status})`);
+        })
       );
+    } else {
+      // choose this api if user select other cathegory (selectValue = "selectCathegory")
+      return this.http
+        .get<Post>(
+          `${this.apiForSelectedCategory}${this.memService.selectedCategory.value}`
+        )
+        .pipe(
+          catchError((error: Response) => {
+            throw new Error(
+              `Błąd sieci: ${error.statusText} (${error.status})`
+            );
+          })
+        );
     }
   }
 }
