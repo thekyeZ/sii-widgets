810d1d72322e3033e7116783b084af55136433b1
Barbara Kryszkiewicz <bkryszkiewicz@sii.pl>
2023-03-15 11:52:03 +0100

    local storage

--- src/app/widgets/dogs/breeds/selected-breed.service.ts
+++ src/app/widgets/dogs/breeds/selected-breed.service.ts
@@ -1,13 +1,76 @@
-import { Injectable } from '@angular/core';
+import { Injectable, } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { LocalStorageService } from '../cat-admin/local-storage-cat.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SelectedBreedService {
 
-  breedSelected: BehaviorSubject<string> = new BehaviorSubject('abys');
+  //ROZWIAZANIE 1 - NIE DZIALA DEFAULT 
+
+  defaultCatBreedStoragedValue = this.localStorageServ.getItem('defaultCatBreed');
+
+  default = 'abys';
+
+  breedSelected: BehaviorSubject<string> = new BehaviorSubject(this.defaultCatBreedStoragedValue || 'abys');
 
 
-  constructor() { }
+  constructor(
+    private localStorageServ: LocalStorageService
+  ) { console.log(this.defaultCatBreedStoragedValue);
+  }
+
+
+  // ROZWIAZANIE 2 PO REFRESH breedSelected is undefined
+
+  // breedSelected: BehaviorSubject<string>;
+
+  //  constructor(
+  //   // private localStorageServ: LocalStorageService 
+  //  ){
+  //   const defaultCatBreedStoragedValue = localStorage.getItem('defaultCatBreed');
+
+  //   this.breedSelected = new BehaviorSubject<string>(defaultCatBreedStoragedValue || 'abys');
+  //  }
+
+  //ROZWIAZANIE 3 
+
+  //   breedSelected: BehaviorSubject<string> = new BehaviorSubject<string>('abys');
+
+  //  constructor(
+  //  ){
+  //   const defaultCatBreedStoragedValue = localStorage.getItem('defaultCatBreed');
+  // if(defaultCatBreedStoragedValue === null) {
+  //   this.breedSelected = new BehaviorSubject<string>(defaultCatBreedStoragedValue);
+  //  } else {
+  //   this.breedSelected = new BehaviorSubject<string>('abys')
+  //  }
+
+  // }
+
+  // ROZWIAZANIE 4 SAME 1 ST SOLUTION (DEFAULT)
+
+ 
+  //   default ='abys';
+
+  //   breedSelected: BehaviorSubject<string>;
+
+  //   constructor(
+  //     private localStorageServ: LocalStorageService
+  //   ){ 
+  //     const defaultCatBreedStoragedValue= this.localStorageServ.getItem('defaultCatBreed');
+
+  //     if(defaultCatBreedStoragedValue === defaultCatBreedStoragedValue.typeOf(Object)) {
+  //       this.breedSelected = new BehaviorSubject<string>(this.default)
+  //       console.log(this.default);
+        
+  //       } else {
+  //       this.breedSelected = new BehaviorSubject<string>(defaultCatBreedStoragedValue)
+      
+  //       }
+  // }
 }
+
+
+
--- src/app/widgets/dogs/cat-admin/cat-admin.component.ts
+++ src/app/widgets/dogs/cat-admin/cat-admin.component.ts
@@ -4,30 +4,33 @@ import { Router } from '@angular/router';
 import { BreedService } from '../breeds/breeds.service';
 import { SelectedBreedService } from '../breeds/selected-breed.service';
 import { Cat } from '../interfaces/cat';
+import { LocalStorageService } from './local-storage-cat.service';
 
 @Component({
   selector: 'app-cat-admin',
   templateUrl: './cat-admin.component.html',
   styleUrls: ['./cat-admin.component.scss']
 })
-export class CatAdminComponent implements OnInit{
-  breedsCats: Cat[]= []
-
+export class CatAdminComponent implements OnInit {
+  breedsCats: Cat[] = []
   catForm: FormGroup = new FormGroup({
     'idCat': new FormControl('', [Validators.required])
   });
+
+  defaultCatBreedL!: string;
 
   constructor(
     private selectedBreed: SelectedBreedService,
     private breedService: BreedService,
     private router: Router,
- ) {}
+    private admin: LocalStorageService,
+  ) { }
 
   ngOnInit() {
-    this.breedService.getBreeds().subscribe(breedApi => {        
+    this.breedService.getBreeds().subscribe(breedApi => {
       this.breedsCats = breedApi;
-      console.log(this.breedsCats);
-      });
+      // console.log(this.breedsCats);
+    });
   }
 
   changeBreed(e: any) {
@@ -39,11 +42,26 @@ export class CatAdminComponent implements OnInit{
   get idCat() {
     return this.catForm.get('idCat');
   }
+
   onSubmit(): void {
-      this.selectedBreed.breedSelected.next(this.catForm.value.idCat!);
-      console.log(this.catForm.value.idCat);
-          this.router.navigate([""]);
-    }
+    // console.log(this.catForm.value.idCat);
+    this.saveData();
+    this.loadData();
+    this.router.navigate([""]);
+
   }
+
+  saveData(){
+    let dataS = this.catForm.value.idCat;
+    this.admin.setItem("defaultCatBreed", dataS);
+  }
+  loadData() {
+    this.defaultCatBreedL = this.admin.getItem("defaultCatBreed");
+    this.selectedBreed.breedSelected.next(this.defaultCatBreedL);
+    console.log(this.defaultCatBreedL);
+    
+  }
+
+}
 
 
