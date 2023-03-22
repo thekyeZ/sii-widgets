67727c1feadaca9a5ca8e4e58d070fdecc75fbcf
Sebastian Nowak <snowak1@sii.pl>
2023-03-22 13:00:11 +0100

    with a draft of custom validator

--- src/app/widgets/weather/weather-admin/weather-admin.component.ts
+++ src/app/widgets/weather/weather-admin/weather-admin.component.ts
@@ -17,6 +17,7 @@ export class WeatherAdminComponent implements OnInit {
   selectedCity!: number;
   cities: CitiesData[] = this.citiesService.cities;
   selectedCityServiceSubscription!: Subscription;
+  availableCountries = ['Poland', 'Polska'];
 
 
   constructor(
@@ -30,6 +31,7 @@ export class WeatherAdminComponent implements OnInit {
     this.adminForm = new FormGroup({
       'city': new FormControl(this.storageCityService.getSelectedCity('storedCity') || '', Validators.required),
       'unit': new FormControl(null, Validators.required),
+      // 'country': new FormControl(null, [Validators.required, this.onWritingCountryName.bind(this)]),
     });
     this.selectedCityServiceSubscription = this.selectedCityService.selectedCity.subscribe(selectedCityFromService => this.selectedCity = selectedCityFromService);
   }
@@ -37,6 +39,14 @@ export class WeatherAdminComponent implements OnInit {
   ngOnDestroy() {
     this.selectedCityServiceSubscription.unsubscribe();
   }
+
+  // onWritingCountryName(control: FormControl): {[s: string]: boolean} {
+  //   // this.availableCountries.indexOf(control.value) ? {'countryIsAvailable': true} : null;
+  //   if (this.availableCountries.indexOf(control.value) !== -1) {
+  //     return {'countryIsAvailable': true};
+  //   }
+  //   return null;
+  // }
 
   onSubmit() {
     this.selectedCityService.selectedCity.next(this.adminForm.get('city')?.value);
