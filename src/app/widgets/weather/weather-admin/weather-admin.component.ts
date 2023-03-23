a2975beecbb76cd27d2b0de3f02de8ad94b95876
Sebastian Nowak <snowak1@sii.pl>
2023-03-23 11:55:32 +0100

    custom validator works

--- src/app/widgets/weather/weather-admin/weather-admin.component.ts
+++ src/app/widgets/weather/weather-admin/weather-admin.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 import { CitiesService } from '../shared/cities.service';
 import { CitiesData} from '../shared/cities-data.model';
 import { SelectedCityService } from '../shared/selected-city.service';
-import { StorageCityService } from '../shared/storage-city.service'
+import { StorageCityService } from '../shared/storage-city.service';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -17,7 +17,7 @@ export class WeatherAdminComponent implements OnInit {
   selectedCity!: number;
   cities: CitiesData[] = this.citiesService.cities;
   selectedCityServiceSubscription!: Subscription;
-  availableCountries = ['Poland', 'Polska'];
+  availableCountries = ['Poland'];
 
 
   constructor(
@@ -31,7 +31,7 @@ export class WeatherAdminComponent implements OnInit {
     this.adminForm = new FormGroup({
       'city': new FormControl(this.storageCityService.getSelectedCity('storedCity') || '', Validators.required),
       'unit': new FormControl(null, Validators.required),
-      // 'country': new FormControl(null, [Validators.required, this.onWritingCountryName.bind(this)]),
+      'country': new FormControl(null, [Validators.required, this.onWritingCountryName.bind(this)]),
     });
     this.selectedCityServiceSubscription = this.selectedCityService.selectedCity.subscribe(selectedCityFromService => this.selectedCity = selectedCityFromService);
   }
@@ -40,13 +40,12 @@ export class WeatherAdminComponent implements OnInit {
     this.selectedCityServiceSubscription.unsubscribe();
   }
 
-  // onWritingCountryName(control: FormControl): {[s: string]: boolean} {
-  //   // this.availableCountries.indexOf(control.value) ? {'countryIsAvailable': true} : null;
-  //   if (this.availableCountries.indexOf(control.value) !== -1) {
-  //     return {'countryIsAvailable': true};
-  //   }
-  //   return null;
-  // }
+  onWritingCountryName(control: FormControl): {[s: string]: boolean } | null {
+    if (this.availableCountries.indexOf(control.value)) {
+      return {'countryIsAvailable': true};
+    }
+    return null;
+  }
 
   onSubmit() {
     this.selectedCityService.selectedCity.next(this.adminForm.get('city')?.value);
